5769ae42232f9243f8b1bb3c22a9478e2d8a3a14
satyansh28 <satyansh.shukla.28@gmail.com>
2022-07-30 19:55:42 +0530

    fix1 uri-mismatch

--- routers/auth_router.js
+++ routers/auth_router.js
@@ -13,7 +13,7 @@ urouter.get("/check",(req,res)=>{
         res.status(200).send();
     }
     else
-        res.status(401).send();
+        res.status(400).send();
 })
 urouter.get("/google",passport.authenticate("google",{scope:["profile","email"]}));
 urouter.get('/google/callback',(req,res,next)=>{console.log("asdone");next()} ,
